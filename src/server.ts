ca010e0dccf6dad08fa5ab8ba5535e1750c006d6
Deyvid-22 <silvadeyvid017@gmail.com>
2025-02-14 16:41:56 -0300

    chore: initial project setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
